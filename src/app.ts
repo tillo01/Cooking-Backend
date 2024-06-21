7e3bd8ef6c81f3689e473bdca67fcbb18f95ac17
tillo01 <xuribayevzakaryo@gmail.com>
2024-06-21 16:52:36 +0900

    feat:integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,14 @@ import router from "./router";
 import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/types/config";
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+    url:String(process.env.MONGO_URL),
+    collection: 'sessions'
+});
 
 /** 1-Entrance */
 const app = express();
@@ -15,10 +23,23 @@ app.use(morgan(MORGAN_FORMAT));
 
     
 /** 2- Sessions */
-app.set("views",path.join(__dirname, "views"));
-app.set("view engine","ejs");
+app.use(
+    session({
+        secret: String(process.env.SESSION_SECRET),
+        cookie:{
+            maxAge:1000 * 3600 * 3, // 3h
+
+        },
+        store:store,
+        resave:false,
+        saveUninitialized:true,
+    })
+);
+
 
 /** 3- Views */
+app.set("views",path.join(__dirname, "views"));
+app.set("view engine","ejs");
 
 
 /** 4- Routers */  
