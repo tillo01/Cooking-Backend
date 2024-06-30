7e0ceb96106f2749ec0392d4c10357fdac7c1cfe
tillo01 <xuribayevzakaryo@gmail.com>
2024-06-30 19:09:59 +0900

    feat: frontend validation and image upload validation logic

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,7 +1,28 @@
 console.log("Signup frontend javascript file");
 
 $(function(){
-    
+    const fileTarget = $(".file-box .upload-hidden");
+    let filename;
+
+    fileTarget.on("change",function(){
+        if (window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            const fileType = uploadFile["type"]; // check you are choosen the right variables 
+             const validImageType =["image/jpeg","image/jpg","image/png"];
+            if(!validImageType){
+alert("Please insert only jpeg and png");
+            }else {
+                if(uploadFile);
+                console.log(URL.createObjectURL(uploadFile));
+$(".upload-img-frame").attr("src",URL.createObjectURL(uploadFile)).addClass("succes");
+                
+
+            }
+            filename = $(this)[0].files[0].name;
+        }
+        $(this).siblings(".upload-name").val(filename);
+
+    });
 
 });
 
@@ -18,6 +39,12 @@ function validateSignupform(){
     if (memberPassword !== confirmPassword) {
         alert("Password differs, please check !");
         return false;
+    }
+    const memberImage = $(".member-image").get(0).files[0] ? $(".member-image").get(0).files[0].name :null;
+    if (!memberImage) {
+        alert("Please restaurant image");
+        return false;
+        
     }
 
 }
