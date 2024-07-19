da2bcaa8b82a6b35343910046eade9aef76b6e11
tillo01 <xuribayevzakaryo@gmail.com>
2024-07-19 18:58:31 +0900

    feat:develop authentication via tokens with cookies

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import  express  from "express";
+/** @format */
+
+import express from "express";
 import path from "path";
 import router from "./router";
 import routerAdmin from "./router-admin";
@@ -7,50 +9,48 @@ import { MORGAN_FORMAT } from "./libs/types/config";
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
 import { T } from "./libs/types/common";
+import cookieParser from "cookie-parser";
 
 const MongoDBStore = ConnectMongoDB(session);
 const store = new MongoDBStore({
-    uri:String (process.env.MONGO_URL),
-    collection: 'sessions'
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
 });
 
 /** 1-Entrance */
 const app = express();
 console.log("__dirname:", __dirname);
-app.use(express.static(path.join(__dirname,"public")));
-app.use(express.urlencoded({extended:true})); 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan(MORGAN_FORMAT));
 
-    
 /** 2- Sessions */
 app.use(
-    session({
-        secret: String(process.env.SESSION_SECRET),
-        cookie:{
-            maxAge:1000 * 3600 * 6 // 3h
-
-        },
-        store:store,
-        resave:true,
-        saveUninitialized:true,
-    })
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 6, // 3h
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
 );
 
-app.use(function(req,res,next){
-    const sessionInstance = req.session as T;
-    res.locals.member = sessionInstance.member;
-next();
+app.use(function (req, res, next) {
+  const sessionInstance = req.session as T;
+  res.locals.member = sessionInstance.member;
+  next();
 });
 
-
 /** 3- Views */
-app.set("views",path.join(__dirname, "views"));
-app.set("view engine","ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+/** 4- Routers */
+app.use("/admin", routerAdmin); // SSR : EJS
+app.use("/", router); // SPA: REACT
 
-/** 4- Routers */  
-app.use("/admin",routerAdmin); // SSR : EJS
-app.use("/",router); // SPA: REACT
-
-export default app; 
+export default app;
--- src/controller/member.controller.ts
+++ src/controller/member.controller.ts
@@ -4,8 +4,9 @@ import express, { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import MemberService from "../models/Member.service";
-import Errors from "../libs/types/Errors";
+import Errors, { HttpCode } from "../libs/types/Errors";
 import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/types/config";
 
 const memberService = new MemberService();
 const authService = new AuthService();
@@ -20,9 +21,9 @@ memberController.signup = async (req: Request, res: Response) => {
       result: Member = await memberService.signup(input);
     const token = await authService.createToken(result);
 
-    console.log("tokens=>", token);
+    res.cookie("accesToken", token, { maxAge: AUTH_TIMER * 3600 * 1000, httpOnly: false });
 
-    res.json({ member: result });
+    res.status(HttpCode.CREATED).json({ member: result, accesToken: token });
   } catch (err) {
     console.log("Error signup", err);
     if (err instanceof Errors) res.status(err.code).json(err);
@@ -38,9 +39,9 @@ memberController.login = async (req: Request, res: Response) => {
       token = await authService.createToken(result);
     console.log("tokens =>", token);
 
-    res.json({ member: result });
+    res.cookie("accesToken", token, { maxAge: AUTH_TIMER * 3600 * 1000, httpOnly: false });
 
-    //  TO DO TOKENS
+    res.status(HttpCode.OK).json({ member: result, accesToken: token });
   } catch (err) {
     console.log("Error, on login", err);
     if (err instanceof Errors) res.status(err.code).json(err);
