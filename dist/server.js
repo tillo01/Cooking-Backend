a423d7707475f62aeda4a1eb2b18f92c44609bfa
Tillochik01 <xuribayevzakaryo@gmail.com>
2025-01-11 00:14:35 +0900

    fix: fixed orders have been fixed

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,21 @@
 "use strict";
+/** @format */
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hello typescriptqs!");
-const moment_1 = __importDefault(require("moment"));
-const currentTime = (0, moment_1.default)().format("YYYY.MM.DD");
-console.log(currentTime);
-const person = ("Kevin");
-const count = 100;
-console.log(person);
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const mongoose_1 = __importDefault(require("mongoose"));
+const app_1 = __importDefault(require("./app"));
+mongoose_1.default
+    .connect(process.env.MONGO_URL, {})
+    .then((data) => {
+    console.log("Mongodb connection successed");
+    const PORT = process.env.PORT ?? 3003;
+    app_1.default.listen(PORT, function () {
+        console.log(`The server is running successfully on port: ${PORT}`);
+        console.log(`Admin Project on http://localhost:${PORT}/admin \n`);
+    });
+})
+    .catch((err) => console.log("Failed on connection mongodb ", err));
