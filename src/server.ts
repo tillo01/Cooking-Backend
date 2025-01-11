a21c0c917400e317ede7b28624ad4fd467bb56c5
Tillochik01 <xuribayevzakaryo@gmail.com>
2025-01-11 18:06:00 +0900

    fix:.env prod

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 /** @format */
 
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({
+   path: process.env.NODE_ENV === "production" ? ".env.production" : ".env",
+});
 import mongoose from "mongoose";
-import app from "./app";
+import server from "./app";
 mongoose
    .connect(process.env.MONGO_URL as string, {})
    .then((data) => {
       console.log("Mongodb connection successed");
       const PORT = process.env.PORT ?? 3008;
-      app.listen(PORT, function () {
+      server.listen(PORT, function () {
          console.log(`The server is running successfully on port: ${PORT}`);
          console.log(`Admin Project on http://localhost:${PORT}/admin \n`);
       });
